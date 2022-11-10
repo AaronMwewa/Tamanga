e9430534887717464df4261e20fe82cd6149a1e8
AaronMwewa <97737768+AaronMwewa@users.noreply.github.com>
2022-11-10 15:12:03 +0200

    Update animate.js

--- lesgo/animate.js
+++ lesgo/animate.js
@@ -5,11 +5,12 @@ let tl = gsap.timeline({defaults:{duration :2}});
 tl.from('.header',{
     'top':'-100%',
     ease:'back.out(.7)',
-    duration:1.5,
+    duration:2,
 })
 
 tl.from('.banner',{
     "clip-path":"polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+    "top":"10%",
     "top":"10%",
     duration:1.6,
 },"-=0")
